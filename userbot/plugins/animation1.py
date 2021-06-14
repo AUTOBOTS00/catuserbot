b71ead661a220dea0306c1176a3b989fe2e24dc9
AUTOBOTS00 <85267187+AUTOBOTS00@users.noreply.github.com>
2021-06-14 07:01:58 +0530

    Update animation1.py

--- userbot/plugins/animation1.py
+++ userbot/plugins/animation1.py
@@ -136,7 +136,7 @@ async def _(event):
         "------------------>",
         "------>;(^。^)ノ",
         "(￣ー￣) DEAD",
-        "`Targeted user killed by Headshot 😈.😈.😈.😈.😈.😈.😈......`\n '#Sad_Reacts_Online'\n",
+        "`Targeted user killed by Headshot 😈.😈.😈.😈.😈.😈.😈......`\n 'SETH PO 😑'\n",
     ]
     for i in animation_ttl:
         await asyncio.sleep(animation_interval)
